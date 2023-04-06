d664176175da599bc42f347b74fe500609a72c21
Francesco Alagna <s265350@studenti.polito.it>
2023-04-06 16:50:58 +0200

    load scripts

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from steamauth import *
+from django.contrib.auth import login, logout
+from .user import process
+from django.conf import settings
+
+USE_SSL = getattr(settings, 'USE_SSL', False)
 
 # Create your views here.
+def signin(request): # GET /login
+    return auth('/api/users/callback', use_ssl=USE_SSL)
+
+def callback(request): # GET /process
+    steam_id = get_uid(request.GET)
+    user = process(steam_id)
+
+    if user is not None:
+        # login success
+        login(request, user)
+    # login failed
+    return redirect('/')
+
+def signout(request):
+    logout(request)
+    return redirect('/')
