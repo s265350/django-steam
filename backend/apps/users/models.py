bf30d05a7b8ee3d37f14665afb5ee760e9d36071
Francesco Alagna <s265350@studenti.polito.it>
2023-04-06 17:20:51 +0200

    Update models.py

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class User(AbstractUser):
@@ -36,6 +39,34 @@ class User(AbstractUser):
         if not self.pk: # primary key
             self.role = self.base_role
             return super().save(*args, **kwargs)
+
+class SteamUserManager(BaseUserManager):
+    # handles operations on SteamUser only
+    def _create_user(self, username, **extra_fields):
+        '''
+        Creates and saves a User with the given steam_id
+        '''
+        try: # python social auth provides an empty email param, which cannot be used here
+            del extra_fields['email']
+        except KeyError:
+            pass
+        
+        if not username:
+            raise ValueError('The given Steam ID must be set')
+
+        user = self.model(username=username, steam_id=username, **extra_fields)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, **extra_fields)
+
+    # returns all steam users
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.STEAMUSER)
 
 class SteamUser(User):
     pass
