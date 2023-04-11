f231edb4b08672219a931afa6e709045eab7b2db
Francesco Alagna <s265350@studenti.polito.it>
2023-04-11 10:38:06 +0200

    Update runwithoutdocker.sh

--- backend/runwithoutdocker.sh
+++ backend/runwithoutdocker.sh
@@ -5,28 +5,33 @@ set -o pipefail # prevents errors in a pipeline from being masked
 set -o nounset #  exit when your script tries to use undeclared variables
 set -o xtrace # trace what gets executed (useful for debugging)
 
+export $(grep -v '^#' ../.env | xargs)
+
 pip install --upgrade pip
 
 pip install virtualenv
 
-virtualenv env
+virtualenv venv
 
-. env/bin/activate
+. venv/bin/activate
 
-pip install -r ./requirements.txt
-
-export $(grep -v '^#' ../.env | xargs)
+pip install -r ./backend/requirements.txt
 
 if [ ${USE_POSTGRES} = 'True' ]
 then
     docker run --name ${POSTGRES_NAME} -p 4321:${POSTGRES_PORT} -e POSTGRES_USER=${POSTGRES_USER} -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} -d postgres
 fi
 
-python manage.py makemigrations users
-python manage.py makemigrations
-python manage.py migrate users
-python manage.py migrate
+python backend/manage.py makemigrations users
 
-# python manage.py collectstatic --noinput --verbosity 0
+python backend/manage.py makemigrations
 
-bash ./scripts/run.sh
+python backend/manage.py migrate
+
+Collect the static files:
+
+python backend/manage.py collectstatic --noinput --verbosity 0
+
+Run the server:
+
+bash ./backend/scripts/run.sh True
